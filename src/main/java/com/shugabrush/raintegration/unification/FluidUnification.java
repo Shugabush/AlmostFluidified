d5b626ba4dab80a95257369d73918363d3b0ffe5
Shugabrush <alexwood319@gmail.com>
2026-04-09 15:03:27 -0700

    Initial commit

---
+++ src/main/java/com/shugabrush/raintegration/unification/FluidUnification.java
@@ -0,0 +1,14 @@
+package com.shugabrush.raintegration.unification;
+
+import net.minecraftforge.fluids.FluidStack;
+
+public class FluidUnification {
+    public static FluidStack getUnifiedFluid(FluidStack fluid) {
+
+        if (fluid.getTag().toString() == "forge:steam") {
+
+        }
+
+        return fluid;
+    }
+}
