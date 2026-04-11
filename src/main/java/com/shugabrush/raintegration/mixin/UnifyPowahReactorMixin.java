03bfe18f4564f6bdf06077547cfaa8a42f8ec6dc
Shugabrush <alexwood319@gmail.com>
2026-04-11 16:37:51 -0700

    Powah finally works completely!

--- src/main/java/com/shugabrush/raintegration/mixin/UnifyPowahReactorMixin.java
+++ src/main/java/com/shugabrush/raintegration/mixin/UnifyPowahReactorMixin.java
@@ -1,8 +1,11 @@
 package com.shugabrush.raintegration.mixin;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 import com.shugabrush.raintegration.ConfigHolder;
 import dev.architectury.hooks.item.ItemStackHooks;
@@ -10,21 +13,35 @@ import dev.architectury.registry.fuel.FuelRegistry;
 import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
 import owmii.powah.api.PowahAPI;
+import owmii.powah.block.reactor.ReactorBlock;
 import owmii.powah.block.reactor.ReactorTile;
-import owmii.powah.item.Itms;
+import owmii.powah.lib.block.AbstractEnergyProvider;
+import owmii.powah.lib.block.IInventoryHolder;
 import owmii.powah.lib.logistics.energy.Energy;
+import owmii.powah.lib.util.Ticker;
 
 @Mixin(value = ReactorTile.class, remap = false)
-public class UnifyPowahReactorMixin {
+public class UnifyPowahReactorMixin extends AbstractEnergyProvider<ReactorBlock> implements IInventoryHolder {
 
-    /**
-     * @author Shugabrush
-     * @reason Allow items with the configured tag to be inserted into the fuel slot
-     */
-    @Overwrite
+    @Shadow
+    public final Ticker fuel = new Ticker(1000);
+
+    @Shadow
+    private int baseTemp;
+
+    public UnifyPowahReactorMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public int getSlotLimit(int i) {
+        return 64;
+    }
+
+    // Allow items with the configured tag to be inserted into the fuel slot
+    @Override
     public boolean canInsert(int slot, ItemStack stack) {
         if (slot == 1) {
             return stack.getTags().toList().contains(ConfigHolder.instance.machineConfigs.getPowahReactorFuel());
@@ -39,15 +56,27 @@ public class UnifyPowahReactorMixin {
             return Energy.chargeable(stack);
     }
 
-    // If the item has the configured uraninite tag, return Powah's uraninite
-    // so it works with the processFuel() method
-    @Redirect(method = "processFuel",
-              at = @At(value = "INVOKE",
-                       target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"))
-    private Item unifiedUraninite(ItemStack originalValue) {
-        if (originalValue.getTags().toList().contains(ConfigHolder.instance.machineConfigs.getPowahReactorFuel())) {
-            return Itms.URANINITE.get();
+    @Override
+    public boolean canExtract(int i, ItemStack itemStack) {
+        return true;
+    }
+
+    @Overwrite
+    private boolean processFuel(Level world) {
+        boolean flag = false;
+        if (this.fuel.getTicks() <= 900) {
+            ItemStack stack = this.inv.getStackInSlot(1);
+            if (stack.getTags().toList().contains(ConfigHolder.instance.machineConfigs.getPowahReactorFuel())) {
+                this.fuel.add(100);
+                this.baseTemp = 700;
+                stack.shrink(1);
+                flag = true;
+            }
         }
-        return originalValue.getItem();
+
+        if (this.fuel.isEmpty()) {
+            this.baseTemp = 0;
+        }
+        return flag;
     }
 }
