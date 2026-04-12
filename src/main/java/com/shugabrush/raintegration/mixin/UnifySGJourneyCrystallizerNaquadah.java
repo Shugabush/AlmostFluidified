b1d6c4a96f2f61fd8ec980c72254d2f381f1b9af
Shugabrush <alexwood319@gmail.com>
2026-04-12 12:35:01 -0700

    SGJourney's crystallizer recipes are unified

--- src/main/java/com/shugabrush/raintegration/mixin/UnifySGJourneyCrystallizerNaquadah.java
+++ src/main/java/com/shugabrush/raintegration/mixin/UnifySGJourneyCrystallizerNaquadah.java
@@ -1,13 +1,14 @@
-package com.shugabrush.raintegration.mixin.industrialforegoing;
+package com.shugabrush.raintegration.mixin;
 
+import com.shugabrush.raintegration.unification.ItemUnification;
 import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.fml.ModList;
 import net.povstalec.sgjourney.common.compatibility.jei.CrystallizerRecipeCategory;
 import net.povstalec.sgjourney.common.recipe.CrystallizerRecipe;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(value = CrystallizerRecipeCategory.class, remap = false)
@@ -17,11 +18,18 @@ public class UnifySGJourneyCrystallizerNaquadah
             at = @At("HEAD"), index = 2)
     private CrystallizerRecipe getUnifiedRecipe(CrystallizerRecipe recipe)
     {
-        NonNullList<Ingredient> ingredients = recipe.getIngredients();
-        ingredients.forEach(ingredient ->
+        if (ModList.get().isLoaded("almostunified"))
         {
-
-        });
+            NonNullList<Ingredient> ingredients = recipe.getIngredients();
+            for (int i = 0; i < ingredients.stream().count(); i++) {
+                ItemStack[] items = ingredients.get(i).getItems();
+                for (int j = 0; j < items.length; j++)
+                {
+                    ItemStack item = new ItemStack(ItemUnification.getItem(items[j]));
+                    recipe.getIngredients().get(i).getItems()[j] = item;
+                }
+            }
+        }
         return recipe;
     }
 }
