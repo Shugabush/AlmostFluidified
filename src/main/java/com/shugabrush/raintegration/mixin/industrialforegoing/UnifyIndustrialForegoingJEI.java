0970c58c9e7d4af0fb165b83bc8673c2c80990d0
Shugabrush <alexwood319@gmail.com>
2026-04-12 10:20:07 -0700

    JEI latex rubber output works now

--- src/main/java/com/shugabrush/raintegration/mixin/industrialforegoing/UnifyIndustrialForegoingJEI.java
+++ src/main/java/com/shugabrush/raintegration/mixin/industrialforegoing/UnifyIndustrialForegoingJEI.java
@@ -2,12 +2,12 @@ package com.shugabrush.raintegration.mixin.industrialforegoing;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.fml.ModList;
 
 import com.almostreliable.unified.AlmostUnified;
 import com.buuz135.industrial.plugin.jei.JEICustomPlugin;
 import com.buuz135.industrial.plugin.jei.machineproduce.MachineProduceWrapper;
-import com.shugabrush.raintegration.RAIntegration;
 import com.shugabrush.raintegration.unification.ItemUnification;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,21 +22,27 @@ public class UnifyIndustrialForegoingJEI {
                at = @At(value = "INVOKE",
                         target = "Lmezz/jei/api/registration/IRecipeRegistration;addRecipes(Lmezz/jei/api/recipe/RecipeType;Ljava/util/List;)V"),
                index = 1)
-    List<MachineProduceWrapper> v(List<MachineProduceWrapper> wrappers) {
+    List<MachineProduceWrapper> getWrappers(List<MachineProduceWrapper> wrappers) {
         if (!ModList.get().isLoaded("almostunified")) return wrappers;
 
-        for (int i = 0; i < wrappers.size(); i++) {
-            MachineProduceWrapper wrapper = wrappers.get(i);
-            ItemStack[] items = wrapper.getOutputItem().getItems();
-            for (int j = 0; j < items.length; j++) {
-                ItemStack item = items[j];
-                ResourceLocation rubberLocation = new ResourceLocation("industrialforegoing:dryrubber");
-                if (item.is(ItemUnification.getItem(rubberLocation))) {
-                    items[j] = new ItemStack(ItemUnification.getItem(AlmostUnified.getRuntime().getReplacementMap()
-                            .get().getReplacementForItem(rubberLocation)));
-                    RAIntegration.LOGGER.info(items[j]);
+        for (int i = 0; i < wrappers.size(); ++i) {
+            try {
+                MachineProduceWrapper wrapper = wrappers.get(i);
+                Ingredient outputItems = wrapper.getOutputItem();
+                if (outputItems != null) {
+                    ItemStack[] items = outputItems.getItems();
+                    for (int j = 0; j < items.length; j++) {
+                        ItemStack item = items[j];
+                        ResourceLocation rubberLocation = new ResourceLocation("industrialforegoing:dryrubber");
+                        if (item.is(ItemUnification.getItem(rubberLocation))) {
+                            item = new ItemStack(ItemUnification.getItem(AlmostUnified.getRuntime().getReplacementMap()
+                                    .get().getReplacementForItem(rubberLocation)));
+                        }
+                        wrapper.getOutputItem().getItems()[j] = item;
+                    }
                 }
-                wrappers.set(i, new MachineProduceWrapper(wrapper.getBlock(), wrapper.getOutputItem().getItems()));
+            } catch (Exception e) {
+
             }
         }
         return wrappers;
