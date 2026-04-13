7eabadc8e0b5672d8454cc4897727cd9c856f76a
Shugabrush <alexwood319@gmail.com>
2026-04-12 17:52:30 -0700

    Update UnifySophisticatedCoreXPMixin.java

--- src/main/java/com/shugabrush/raintegration/mixin/UnifySophisticatedCoreXPMixin.java
+++ src/main/java/com/shugabrush/raintegration/mixin/UnifySophisticatedCoreXPMixin.java
@@ -16,10 +16,6 @@ public class UnifySophisticatedCoreXPMixin {
                         target = "Lnet/p3pp3rf1y/sophisticatedcore/api/IStorageFluidHandler;fill(Lnet/minecraft/tags/TagKey;ILnet/minecraft/world/level/material/Fluid;Lnet/minecraftforge/fluids/capability/IFluidHandler$FluidAction;Z)I"),
                index = 2)
     private Fluid unifiedXp(Fluid originalFluid) {
-        Fluid unifiedFluid = MoreUnification.getReplacementForFluid(originalFluid);
-        if (unifiedFluid != null) {
-            return unifiedFluid;
-        }
-        return originalFluid;
+        return MoreUnification.getReplacementForFluid(originalFluid);
     }
 }
