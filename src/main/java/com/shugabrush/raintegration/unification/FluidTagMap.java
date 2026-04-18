519af43e2bb71761d7b169f909b3266f1394b731
Shugabrush <alexwood319@gmail.com>
2026-04-18 13:07:49 -0700

    Update FluidTagMap.java

--- src/main/java/com/shugabrush/raintegration/unification/FluidTagMap.java
+++ src/main/java/com/shugabrush/raintegration/unification/FluidTagMap.java
@@ -2,14 +2,12 @@ package com.shugabrush.raintegration.unification;
 
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluid;
 
 import com.almostreliable.unified.utils.TagMap;
 import com.almostreliable.unified.utils.UnifyTag;
 
 import java.util.Collection;
-import java.util.Map;
 
 public class FluidTagMap extends TagMap< Fluid>
 {
