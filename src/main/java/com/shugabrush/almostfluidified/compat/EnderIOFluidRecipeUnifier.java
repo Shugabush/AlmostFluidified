e6db09b8441a466192580e0dc040c6d75d5629a5
Shugabrush <alexwood319@gmail.com>
2026-04-27 08:10:48 -0700

    Fixed Ender IO Fluid Recipe Unification

--- src/main/java/com/shugabrush/almostfluidified/compat/EnderIOFluidRecipeUnifier.java
+++ src/main/java/com/shugabrush/almostfluidified/compat/EnderIOFluidRecipeUnifier.java
@@ -1,9 +1,18 @@
 package com.shugabrush.almostfluidified.compat;
 
+import net.minecraft.resources.ResourceLocation;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.shugabrush.almostfluidified.api.FluidRecipeUnifier;
 import com.shugabrush.almostfluidified.api.FluidRecipeUnifierBuilder;
+import com.shugabrush.almostfluidified.unification.FluidRecipeContext;
 
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class EnderIOFluidRecipeUnifier implements FluidRecipeUnifier
 {
@@ -12,6 +21,42 @@ public class EnderIOFluidRecipeUnifier implements FluidRecipeUnifier
     public void collectUnifier(FluidRecipeUnifierBuilder builder)
     {
         List.of("fluid")
-                .forEach(key -> builder.put(key, (json, ctx) -> ctx.createIngredientReplacement(json)));
+                .forEach(key -> builder.put(key, this::createIngredientReplacement));
+    }
+
+    private JsonElement createIngredientReplacement(@Nullable JsonElement element, FluidRecipeContext context)
+    {
+        if (element == null)
+            return null;
+
+        JsonElement copy = element.deepCopy();
+        tryCreateIngredientReplacement(copy, context);
+        return element.equals(copy) ? null : copy;
+    }
+
+    // Don't replace with tag, just use unified fluid
+    private void tryCreateIngredientReplacement(@Nullable JsonElement element, FluidRecipeContext context)
+    {
+        if (element == null)
+            return;
+        if (element instanceof JsonArray array)
+        {
+            for (JsonElement e : array)
+            {
+                tryCreateIngredientReplacement(e, context);
+            }
+        }
+        else if (element instanceof JsonObject object)
+        {
+            if (object.get("fluid") instanceof JsonPrimitive primitive)
+            {
+                ResourceLocation fluid = ResourceLocation.tryParse(primitive.getAsString());
+                ResourceLocation unifiedFluid = context.getReplacementForFluid(fluid);
+                if (unifiedFluid != null)
+                {
+                    object.addProperty("fluid", unifiedFluid.toString());
+                }
+            }
+        }
     }
 }
